150cb0a7a95e062f4251deb9d653abbb77ec5a91
haikalmumtaz233 <123210062@student.upnyk.ac.id>
2023-11-19 03:33:02 +0700

    adding files

---
+++ script.js
@@ -0,0 +1,14 @@
+const navbarIcon = document.querySelector("#navbar-icon");
+navbarIcon.addEventListener("click", function () {
+    const navbarGroup = document.querySelector("#navbar-group");
+    navbarGroup.classList.toggle("navbar-group--close");
+});
+
+const navbarItems = document.querySelectorAll("#navbar-group li");
+
+for (const item of navbarItems) {
+    item.addEventListener("click", function () {
+        const navbarGroup = document.querySelector("#navbar-group");
+        navbarGroup.classList.add("navbar-group--close");
+    });
+}
